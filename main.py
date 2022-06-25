6b4f42dcd6d1a140f0f9b3f559e797039e8ee9f0
DoguCigsar <dogucigsar@hotmail.com>
2022-06-25 11:52:32 +0300

    v2.1 Send The Number Of Messages Sent

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-#Advanced Spam Tool v2.0 Made by DoguCigsar
-#This tool works with any messaging app that uses the enter key as send
+# Advanced Spam Tool v2.1 Made by DoguCigsar
+# This tool works with any messaging app that uses the enter key as send
 
 from pynput.keyboard import Key, Controller
 import time
 
 keyboard = Controller()
 
-print("<--Welcome to Advanced Spam Tool v2.0 Made by DoguCigsar-->")
+print("<--Welcome to Advanced Spam Tool v2.1 Made by DoguCigsar-->")
 print("<--This tool works with any messaging app that uses the enter key as send-->")
 
-message = input("The message you want to spam (If left blank, the program will spam the letter A): ")
+message = input("The message you want to spam (If left blank, the program will spam the letter A) (If you want to "
+                "spam the amount of messages that are sent just type 'message amount'): ")
 amount = input("Amount of messages to send (If left blank the program will send 1000 messages) : ")
 delay = input("Delay between messages (If left blank the program will send messages with a delay of 0.1 seconds) : ")
+
 
 def warningSequence():
     print("You have 10 seconds to select the messaging app!!! Also consider if you really want to do this!!!")
@@ -36,10 +38,12 @@ def warningSequence():
     print("1")
     time.sleep(1)
     print("GO!")
+
+
 def main(delay, amount):
     global message
-    x = 0
-    
+    x = 1
+
     if (amount == ""):
         amount = 1000
     else:
@@ -48,15 +52,21 @@ def main(delay, amount):
         delay = 0.1
     else:
         delay = float(delay)
-    if(message == ""):
+    if (message == ""):
         message = "A"
         messageLen = len(message)
+    elif (message == "message amount"):
+        numMode = True
+        message = ""
     else:
         messageLen = len(message)
     warningSequence()
-    while x < amount:
+    while x <= amount:
         currentLetter = 0
-        while currentLetter <= messageLen-1:
+        if (numMode == True):
+            message = str(x)
+            messageLen = len(message)
+        while currentLetter <= messageLen - 1:
             keyboard.press(message[currentLetter])
             keyboard.release(message[currentLetter])
             currentLetter = currentLetter + 1
@@ -65,5 +75,6 @@ def main(delay, amount):
         x = x + 1
         print(x)
         time.sleep(delay)
+
 
 main(delay, amount)
